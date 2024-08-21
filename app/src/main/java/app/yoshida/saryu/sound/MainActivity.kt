aacd5573d63409eff7e4f8d5b1ac11bbfd62dddb
KENKUN-1031 <kencoding1031@gmail.com>
2024-08-21 21:44:47 +0900

    finalCommit

--- app/src/main/java/app/yoshida/saryu/sound/MainActivity.kt
+++ app/src/main/java/app/yoshida/saryu/sound/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
+        //ドラムに関する処理 ↓ (将来関数 or クラス化したい！)
         val drumSound: MediaPlayer = MediaPlayer.create(this, R.raw.drum_sound)
         binding.drumImage.setOnTouchListener { view, motionEvent ->
 
@@ -24,15 +25,36 @@ class MainActivity : AppCompatActivity() {
                 binding.drumImage.setImageResource(R.drawable.drum_playing_image) //写真を置き換えてる
                 drumSound.seekTo(0) //←これ書いたら連続で音出る(具体的になんで？？？知りたい！！)
                 drumSound.start()
-            }
-            else if (motionEvent.action == MotionEvent.ACTION_UP) {
+            } else if (motionEvent.action == MotionEvent.ACTION_UP) {
                 binding.drumImage.setImageResource(R.drawable.drum_image)
             }
             true
         }
-//        binding.drumImage.setOnClickListener {
-//            drumSound.seekTo(0)
-//            drumSound.start()
-//        }
+
+        //ピアノに関する処理↓
+        val pianoSound = MediaPlayer.create(this, R.raw.piano_sound)
+        binding.pianoImage.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                binding.pianoImage.setImageResource(R.drawable.piano_playing_image)
+                pianoSound.seekTo(0)
+                pianoSound.start()
+            } else if (motionEvent.action == MotionEvent.ACTION_UP) {
+                binding.pianoImage.setImageResource(R.drawable.piano_image)
+            }
+            true
+        }
+
+        //ギターに関する処理↓
+        val guitarSound = MediaPlayer.create(this, R.raw.guitar_sound)
+        binding.guitarImage.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                binding.guitarImage.setImageResource(R.drawable.guitar_playing_image)
+                guitarSound.seekTo(0)
+                guitarSound.start()
+            } else if (motionEvent.action == MotionEvent.ACTION_UP) {
+                binding.guitarImage.setImageResource(R.drawable.guitar_image)
+            }
+            true
+        }
     }
 }
